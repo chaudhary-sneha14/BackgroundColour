60e2859fa80f8f05df988a26893a94c3d2585737
chaudhary-sneha14 <snehachaudhary1411@gmail.com>
2024-09-11 00:43:40 +0530

    Add files via upload

---
+++ ChangeBackground/script.js
@@ -0,0 +1,27 @@
+let red = document.getElementById("red")
+let green = document.getElementById("green")
+let pink = document.getElementById("pink")
+let yellow = document.getElementById("yellow")
+let blue = document.getElementById("blue")
+
+red.addEventListener("click",()=>{
+          document.body.style.backgroundColor = 'red';
+    
+    })
+green.addEventListener("click",()=>{
+          document.body.style.backgroundColor = 'green';
+     
+    })
+pink.addEventListener("click",()=>{
+          document.body.style.backgroundColor = 'pink';
+     
+    })
+yellow.addEventListener("click",()=>{
+          document.body.style.backgroundColor = 'yellow';
+     
+    })
+blue.addEventListener("click",()=>{
+          document.body.style.backgroundColor = 'blue';
+     
+    })
+
